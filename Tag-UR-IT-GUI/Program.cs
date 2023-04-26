2a819d04100a1c264f66b5629a320bc49e475a26
luna-dzx <luna.l.dalzell@gmail.com>
2023-04-26 21:10:53 +0100

    Project Creation

---
+++ Tag-UR-IT-GUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+
+namespace Tag_Ur_IT_GUI;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var displayMode = new VideoMode(1600, 900);
+        var window = new RenderWindow(displayMode, "Test");
+
+        // actually exit when you press the "X" button
+        window.Closed += (_, _) => window.Close();
+        
+
+        while (window.IsOpen)
+        {
+            window.DispatchEvents();
+            //window.Draw(render);
+            window.Display();
+        }
+        
+    }
+}
